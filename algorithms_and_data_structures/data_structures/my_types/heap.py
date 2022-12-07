de206095ce46a4e7de3eccb330c6e81982a49091
Mateusz Kowalczyk <mkowalczyk013@gmail.com>
2022-12-07 23:13:25 +0100

    Update.

--- algorithms_and_data_structures/data_structures/my_types/heap.py
+++ algorithms_and_data_structures/data_structures/my_types/heap.py
@@ -8,20 +8,20 @@ class Heap:
     """Provides basic example of the heap structure."""
 
     def __init__(self, size):
-        self._length = 0
-        self._heap = [0 for _ in range(size + 1)]
+        self.length = 0
+        self.heap = [0 for _ in range(size + 1)]
 
     def push(self, value):
         """Push a value to the heap."""
-        self._length += 1
-        self._heap[self._length] = value
+        self.length += 1
+        self.heap[self.length] = value
         self._go_to_the_top()
 
     def pop(self):
         """Pop the value from the heap."""
-        value = self._heap[1]
-        self._heap[1] = self._heap[self._length]
-        self._length -= 1
+        value = self.heap[1]
+        self.heap[1] = self.heap[self.length]
+        self.length -= 1
         self._go_to_the_bottom()
         return value
 
@@ -29,35 +29,35 @@ class Heap:
         """Print the heap out."""
         if message:
             print(message)
-        for index in range(1, self._length // 2 + 1):
-            print(f"Parent: {self._heap[index]}", end=" ")
-            print(f"Left Child: {self._heap[2 * index]}", end=" ")
-            if 2 * index + 1 <= self._length:  # Right child can not exist.
-                print(f"Right Child: {self._heap[2 * index + 1]}", end=" ")
+        for index in range(1, self.length // 2 + 1):
+            print(f"Parent: {self.heap[index]}", end=" ")
+            print(f"Left Child: {self.heap[2 * index]}", end=" ")
+            if 2 * index + 1 <= self.length:  # Right child can not exist.
+                print(f"Right Child: {self.heap[2 * index + 1]}", end=" ")
             print()
 
     def _go_to_the_top(self):
         """Realise priority of the pushed value."""
-        n = self._length
-        value = self._heap[self._length]
-        while n != 1 and self._heap[n // 2] <= value:
-            self._heap[n] = self._heap[n // 2]
+        n = self.length
+        value = self.heap[self.length]
+        while n != 1 and self.heap[n // 2] <= value:
+            self.heap[n] = self.heap[n // 2]
             n = n // 2
-        self._heap[n] = value
+        self.heap[n] = value
 
     def _go_to_the_bottom(self):
         """Repair heap priorities after pop."""
         parent = 1
         while True:
             child = 2 * parent  # 2 * parent (left child), 2 * parent + 1 (right child).
-            if child > self._length:
+            if child > self.length:
                 break
-            if child + 1 <= self._length:  # Right child can not exist.
-                if self._heap[child] < self._heap[child + 1]:
+            if child + 1 <= self.length:  # Right child can not exist.
+                if self.heap[child] < self.heap[child + 1]:
                     child += 1
-            if self._heap[parent] > self._heap[child]:
+            if self.heap[parent] > self.heap[child]:
                 break
-            tmp = self._heap[child]
-            self._heap[child] = self._heap[parent]
-            self._heap[parent] = tmp
+            tmp = self.heap[child]
+            self.heap[child] = self.heap[parent]
+            self.heap[parent] = tmp
             parent = child
--- algorithms_and_data_structures/data_structures/my_types/lifo_stack.py
+++ algorithms_and_data_structures/data_structures/my_types/lifo_stack.py
@@ -7,37 +7,37 @@ class LifoStack:
     """Provides basic example of the stack structure."""
 
     def __init__(self, size, stack_type):
-        self._stack = list()
-        self._size = size
-        self._stack_type = type(stack_type())
+        self.stack = list()
+        self.size = size
+        self.stack_type = type(stack_type())
 
     def clear(self):
         """Clear the stack."""
-        self._stack.clear()
+        self.stack.clear()
 
     def print(self, message=None):
         """Print the stack elements."""
         if message:
             print(message)
-        if self._stack:
+        if self.stack:
             print(f"{self.__class__.__qualname__}: [", end=" ")
-            for element in self._stack:
+            for element in self.stack:
                 print(element, end=" ")
             print("]")
 
     def push(self, element):
         """Push a new element to the stack."""
-        if not isinstance(element, self._stack_type):
+        if not isinstance(element, self.stack_type):
             raise TypeError(
-                f"Element must be '{self._stack_type.__qualname__}', "
+                f"Element must be '{self.stack_type.__qualname__}', "
                 f"not '{element.__class__.__qualname__}'.")
-        if len(self._stack) < self._size:
-            self._stack.append(element)
+        if len(self.stack) < self.size:
+            self.stack.append(element)
         else:
             raise Exception("Stack Overflow.")
 
     def pop(self):
         """Pop an element from the stack."""
-        if len(self._stack) > 0:
-            return self._stack.pop()
+        if len(self.stack) > 0:
+            return self.stack.pop()
         raise Exception("Stack is empty.")
